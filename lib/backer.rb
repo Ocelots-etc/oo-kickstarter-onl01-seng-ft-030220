ad0c6a9e27dd12b2e9a56cfdb8ac12ca3ef395a5
Cat Gomez <catherinedgomez@gmail.com>
2020-03-19 23:04:59 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,8 @@
 require 'pry'
 class Backer
 
-<<<<<<< HEAD
 attr_accessor :name, :backers
 attr_reader :backed_projects
-=======
-attr_accessor :name
-attr_reader :backed_projects, :backers
->>>>>>> abffc1f960c28f2cf27caab3e49f790a96bd2b26
 
   def initialize(name)
     @name = name
@@ -16,16 +11,7 @@ attr_reader :backed_projects, :backers
 
   def back_project(title)
     @backed_projects << title
-<<<<<<< HEAD
     title.backers << self
   end
 
-=======
-    Project.backers << title
-  end
-
-
-
-
->>>>>>> abffc1f960c28f2cf27caab3e49f790a96bd2b26
 end
